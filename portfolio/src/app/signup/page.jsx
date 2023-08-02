002ef887215e244e8f0cb95ed55d3a958cedec03
Ashjha75 <113693962+Ashjha75@users.noreply.github.com>
2023-08-02 16:21:57 +0530

    1 file modified

--- portfolio/src/app/signup/page.jsx
+++ portfolio/src/app/signup/page.jsx
@@ -1,7 +1,36 @@
-export default function signupe() {
+export default function signup() {
   return (
-    <>
-      <div className="flex h-screen items-center justify-center ">Signup</div>
-    </>
+    <main>
+      <div className="flex flex-col h-screen items-center justify-center ">
+        <h1>Signup</h1>
+        <form
+          className="flex flex-col m-20 w-[350px] gap-y-5 "
+          action="/send-data-here"
+          method="post"
+        >
+          <label for="first">First name:</label>
+          <input
+            className="p-3 outline-none rounded-sm"
+            placeholder="username"
+            type="text"
+          />
+          <label for="last">Email</label>
+          <input
+            className="p-3 outline-none rounded-sm"
+            type="text"
+            placeholder="email"
+          />
+          <label for="last">Password</label>
+          <input
+            className="p-3 outline-none rounded-sm"
+            type="text"
+            placeholder="password"
+          />
+          <button className="p-2 bg-blue-700 rounded-sm" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
